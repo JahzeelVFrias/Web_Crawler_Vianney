a9dc7105f07a3900e451f489ffaaa68970ad7dd2
Jahzeel Vargas <147675241+JahzeelVFrias@users.noreply.github.com>
2024-05-17 04:03:14 -0600

    Add files via upload
    Se agrego la parte de busca el precio anterior y el de oferta de cada producto.

--- Web_Scraping_Vianey.py
+++ Web_Scraping_Vianey.py
@@ -7,42 +7,47 @@ base_url = 'https://vianney.com.mx/collections/novedades-%F0%9F%94%A5?page={}'
 
 all_data = []
 
-# Pagina inicial 
-page_number = 1
+# Pagina inicial  
+numero_pagina = 1
 
 # Final de las paginas
-end_of_pages = False
+fin_paginas = False
 
+while not fin_paginas:
 
-while not end_of_pages:
-
-    url = base_url.format(page_number)
+    url = base_url.format(numero_pagina)
 
     r = requests.get(url)
-    print(f"Status code for page {page_number}: {r.status_code}")
+    print(f"Pagina {numero_pagina}: {r.status_code}")
     
     soup = BeautifulSoup(r.content, 'html.parser')
     productos = soup.find_all('a', class_='product-info__caption')
 
-
     if not productos:
         print("No hay más páginas disponibles.")
-        end_of_pages = True
+        fin_paginas = True
         continue
 
     for producto in productos:
         titulo = producto.find('span', class_='title').text.strip()
         url_producto = 'https://vianney.com.mx' + producto.get('href')
         precio = producto.find('span', class_='money').text.strip()
-        all_data.append({'TITULO': titulo, 'URLS': url_producto, 'PRECIOS': precio})
+        precio_anterior_etiqueta = producto.find('span', class_='was_price')
+        # Por si no tiene la clase "was_price" el articulo
+        precio_anterior = None
+        if precio_anterior_etiqueta:
+            precio_anterior_span = precio_anterior_etiqueta.find('span', class_='money')
+            if precio_anterior_span:
+                precio_anterior = precio_anterior_span.text.strip()
+        all_data.append({'TITULO': titulo, 'URLS': url_producto, 'PRECIO_OFERTA': precio, 'PRECIO_ANTERIOR': precio_anterior})
 
     # Verificar si el botón de "Siguiente" está presente en la página
     next_button = soup.find('span', class_='next')
     if not next_button:
         print("No hay más páginas disponibles.")
-        end_of_pages = True
+        fin_paginas = True
         continue
-    page_number += 1
+    numero_pagina += 1
 
 
 df = pd.DataFrame(all_data)
@@ -51,10 +56,10 @@ df = pd.DataFrame(all_data)
 print(df)
 
 # (Pongan su direcorio xd)
-df.to_csv('D:/ACTIVIDADES INNI/6to Semestre/Programacion para Internet/Novedades_Vianey.csv', index=False)
+df.to_csv('D:/ACTIVIDADES INNI/6to Semestre/Programacion para Internet/Articulos_Vianey.csv', index=False)
 
 try:
-    df.to_csv('datos_vianney_todas_paginas.csv', index=False)
+    df.to_csv('Articulos_Vianey.csv', index=False)
     print("Archivo CSV generado exitosamente.")
 except Exception as e:
     print("Error al generar el archivo CSV:", e)
